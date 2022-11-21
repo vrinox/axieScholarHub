ee35534e515253315c2e91838d841139434131c6
vrinox <vrinox@hotmail.com>
2022-11-21 12:34:18 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,13 +3,13 @@ export const environment = {
   appVersion: "3.1.1",
   production: false,
   firebase: {
-    apiKey: "AIzaSyD-ZTONH0mNWzNT1-IHBOqdBf4qiYUYfQ8",
+    apiKey: "",
     authDomain: "axieacademytracker.firebaseapp.com",
     databaseURL: "https://axieacademytracker-default-rtdb.firebaseio.com",
     projectId: "axieacademytracker",
     storageBucket: "axieacademytracker.appspot.com",
-    messagingSenderId: "337378328440",
-    appId: "1:337378328440:web:e4b18e8b2afc0af62ed28d",
-    measurementId: "G-FP3K84QLWF"
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
